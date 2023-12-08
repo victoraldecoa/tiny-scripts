608e0f5bd2ddc0edaf2294910d7366e9d142f8ab
Victor Maia Aldecoa <victor.maia.a@gmail.com>
2023-12-08 15:12:56 -0300

    Update weekdays-in-month.clj

--- weekdays-in-month.clj
+++ weekdays-in-month.clj
@@ -10,7 +10,7 @@
 
 (def dictionary
   {:pt {:months [nil "Janeiro" "Fevereiro" "Março" "Abril" "Maio" "Junho" "Julho" "Agosto" "Setembro" "Outubro" "Novembro" "Dezembro"]
-        :days-in-week ["Domingo" "Segunda" "Terça" "Quarta" "Quinta" "Sexta" "Sábado"]}})
+        :days-in-week ["Domingo" "Segunda-feira" "Terça-feira" "Quarta-feira" "Quinta-feira" "Sexta-feira" "Sábado"]}})
 
 (defn day-of-week-int
   [^LocalDate date]
