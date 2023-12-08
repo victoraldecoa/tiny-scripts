a83bf6f0c743d6eb842712d8508eff3bdc9285b0
Victor Maia Aldecoa <victor.maia.a@gmail.com>
2023-12-08 15:01:30 -0300

    weekdays-in-month: Add support for portuguese

--- weekdays-in-month.clj
+++ weekdays-in-month.clj
@@ -8,23 +8,42 @@
 (import '[java.time DayOfWeek LocalDate YearMonth])
 (import '[java.time.format DateTimeFormatter])
 
+(def dictionary
+  {:pt {:months [nil "Janeiro" "Fevereiro" "Março" "Abril" "Maio" "Junho" "Julho" "Agosto" "Setembro" "Outubro" "Novembro" "Dezembro"]
+        :days-in-week ["Domingo" "Segunda" "Terça" "Quarta" "Quinta" "Sexta" "Sábado"]}})
+
+(defn day-of-week-int
+  [^LocalDate date]
+  (.getValue (.getDayOfWeek date)))
+
 (defn weekday?
   [^LocalDate date]
-  (<= (.getValue (.getDayOfWeek date)) (.getValue DayOfWeek/FRIDAY)))
+  (<= (day-of-week-int date) (.getValue DayOfWeek/FRIDAY)))
+
+; babashka does not come with locales other than :en, so we have to build a custom one
+(defn formatted-custom
+  [language ^LocalDate date]
+  (let [dict (dictionary language)
+        days-in-week (dict :days-in-week)
+        months (dict :months)]
+    (str (days-in-week (day-of-week-int date)) ", " (.getDayOfMonth date) " de " (months (.getMonthValue date)) " de " (.getYear date))))
 
 (defn formatted
-  [^LocalDate date]
-  (.format date (DateTimeFormatter/ofPattern "EEEE, MMMM d, yyyy")))
+  [language ^LocalDate date]
+  (case language
+    :en (.format date (DateTimeFormatter/ofPattern "EEEE, MMMM d, yyyy"))
+    (formatted-custom language date)))
 
 (defn str-log
-  [{:keys [month year]}]
+  [{:keys [month year language]}]
   (->> (range 1 (inc (.lengthOfMonth (YearMonth/of year month))))
        (map #(LocalDate/of year month %))
        (filter weekday?)
-       (map formatted)))
+       (map (partial formatted language))))
 
 (def cli-options {:month {:coerce :long}
                   :year {:default (.getYear (LocalDate/now)) :coerce :long}
+                  :language {:default :en :coerce :keyword}
                   :append {:coerce :string}})
 
 (let [{:keys [append] :as args} (cli/parse-opts *command-line-args* {:spec cli-options})]
@@ -33,4 +52,3 @@
         str-log
         (map #(str % append))
         (str/join "\n"))))
-
