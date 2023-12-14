29c21315584e8403827e5c2c0afb6be8ac10658a
Victor Maia Aldecoa <victor.maia.a@gmail.com>
2023-12-14 11:46:47 -0300

    Report final price

--- weekdays-in-month.clj
+++ weekdays-in-month.clj
@@ -25,37 +25,47 @@
   [log {:keys [formatted date]}]
   (str formatted ": 7 hours - " log (when (= (.getDayOfWeek date) DayOfWeek/FRIDAY) "\n")))
 
-(def cli-options {:month {:coerce :long}
-                  :year {:default (.getYear (LocalDate/now)) :coerce :long}
-                  :language {:default :en :coerce :keyword}
-                  :append {:coerce :string}
-                  :file {:coerce :string}})
-
-(defn logged-work
-  [{:keys [from to log month year skip]}]
+(defn working-days
+  [{:keys [from to month year skip]}]
   (->> (range from to)
        (remove skip)
        (map #(LocalDate/of year month %))
-       (filter weekday?)
+       (filter weekday?)))
+
+(defn logged-work
+  [{:keys [log] :as args}]
+  (->> (working-days args)
        (map formatted)
        (map (partial log-form log))
        (str/join "\n")))
 
+(defn- final-total
+  [{:keys [hourly-rate] :as args}]
+  (* 7 hourly-rate (count (working-days args))))
+
 (defn month-work
-  [{:keys [year month] :as args}
-   {:keys [work-log] :as work}]
+  [{:keys [year month work-log hourly-rate] :as args}]
   (let [last-day (.lengthOfMonth (YearMonth/of year month))
         ranges (partition 2 1 (conj (vec (keys work-log)) (inc last-day)))]
-    (->> ranges
-         (map #(logged-work (merge args work {:from (first %)
-                                              :to (last %)
-                                              :log (work-log (first %))})))
-         (str/join "\n"))))
+    (str
+     (->> ranges
+          (map #(logged-work (merge args {:from (first %)
+                                          :to (last %)
+                                          :log (work-log (first %))})))
+          (str/join "\n"))
+     "\nFinal Total / Balance Due: USD " (final-total (merge args {:from 1
+                                                                   :to last-day})))))
+
+(def cli-options {:month {:coerce :long}
+                  :year {:default (.getYear (LocalDate/now)) :coerce :long}
+                  :language {:default :en :coerce :keyword}
+                  :hourly-rate {:coerce :long}
+                  :file {:coerce :string}})
 
 (defn print-report!
   []
   (let [{:keys [file] :as args} (cli/parse-opts *command-line-args* {:spec cli-options})
-        work (read-string (slurp file))]
-    (println (month-work args work))))
+        content (read-string (slurp file))]
+    (println (month-work (merge args content)))))
 
 (print-report!)
